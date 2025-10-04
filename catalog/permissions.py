2dfb4f2181fc254a3a1a26f34bdf5f478a15ebce
Ali Miri <miri03369@gmail.com>
2025-10-04 16:30:05 +0330

    CREAT permissions in catalog app

---
+++ catalog/permissions.py
@@ -0,0 +1,8 @@
+# catalog/permissions.py
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsReviewOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
