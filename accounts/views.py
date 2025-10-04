383dfe9e1e4e2da8aa43d46d2903c14fb85b2314
Ali Miri <miri03369@gmail.com>
2025-10-04 16:05:42 +0330

    add urls for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# accounts/views.py
+from rest_framework import generics, permissions
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterAPIView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+    throttle_scope = "auth"  # Throttle scoped
