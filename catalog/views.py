35f01760b989ed090a0f7f078779905f1cefac76
Ali Miri <miri03369@gmail.com>
2025-10-04 16:25:20 +0330

    CREAT class for show in one views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# catalog/views.py
+from rest_framework import generics, permissions, pagination, filters
+from rest_framework.throttling import ScopedRateThrottle
+from .models import Product, Review, Category
+from .serializers import ProductSerializer, ReviewSerializer, CategorySerializer
 
-# Create your views here.
+# Pagination اختصاصی (برای نمایش در یکی از ویوها)
+class SmallPageNumberPagination(pagination.PageNumberPagination):
+    page_size = 5
+    page_query_param = "page"
+    max_page_size = 50
+
