ba7b1c1804334d7d223be0d8de40072da34146f7
Ali Miri <miri03369@gmail.com>
2025-10-04 16:38:17 +0330

    add serializer

---
+++ orders/serializers.py
@@ -0,0 +1,45 @@
+# orders/serializers.py
+from rest_framework import serializers
+from .models import Order, OrderItem
+from catalog.models import Product
+
+class OrderItemInputSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+    quantity = serializers.IntegerField(min_value=1)
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField()
+    class Meta:
+        model = OrderItem
+        fields = ("product", "quantity", "price")
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ("id", "user", "created_at", "paid", "items")
+        read_only_fields = ("user", "created_at", "items")
+
+class OrderCreateSerializer(serializers.Serializer):
+    items = OrderItemInputSerializer(many=True)
+
+    def validate(self, attrs):
+        # مثال ولیدیشن سطح بالا
+        items = attrs["items"]
+        if len(items) == 0:
+            raise serializers.ValidationError("حداقل یک آیتم لازم است.")
+        return attrs
+
+    def create(self, validated_data):
+        request = self.context["request"]
+        order = Order.objects.create(user=request.user)
+        for it in validated_data["items"]:
+            product = Product.objects.get(pk=it["product_id"], is_active=True)
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=it["quantity"],
+                price=product.price
+            )
+        return order
