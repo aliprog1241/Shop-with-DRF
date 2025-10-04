118af63ff8b3d567339ea63a4034129fb91e9fb1
Ali Miri <miri03369@gmail.com>
2025-10-04 16:37:57 +0330

    add permissions

---
+++ orders/permissions.py
@@ -0,0 +1,6 @@
+# orders/permissions.py
+from rest_framework.permissions import BasePermission
+
+class IsOrderOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
