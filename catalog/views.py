b1a1b6dcd389bfc07ef75fc26295060aae630d5d
Ali Miri <miri03369@gmail.com>
2025-10-04 16:26:37 +0330

    CREAT class for example

--- catalog/views.py
+++ catalog/views.py
@@ -10,3 +10,19 @@ class SmallPageNumberPagination(pagination.PageNumberPagination):
     page_query_param = "page"
     max_page_size = 50
 
+# LimitOffsetPagination نمونه
+class ProductLimitOffsetPagination(pagination.LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
+# CursorPagination نمونه
+class ProductCursorPagination(pagination.CursorPagination):
+    page_size = 10
+    ordering = "-created_at"
+
+class CategoryListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all().order_by("title")
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]  # فقط ادمین بسازد
+    throttle_scope = "products"
+
