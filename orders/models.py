f590bcfb6daff74bc59309229bb010e795ba3452
Ali Miri <miri03369@gmail.com>
2025-10-04 16:36:30 +0330

    add orders models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
+# orders/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name="orders", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order#{self.id} by {self.user}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # snapshot of product.price
